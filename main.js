994f00791d4bf10146a82eb470306eb93798cc87
charlyx125 <joyce99.jc@gmail.com>
2026-07-08 14:29:58 +0100

    style app with pixel fonts, dreamy pastel gradient background

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
 // main.js — Electron's main process. Its job is to create the app window.
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow } = require("electron");
 
 function createWindow() {
   const win = new BrowserWindow({
-    width: 900,
-    height: 700,
+    width: 450,
+    height: 500,
   });
 
-  win.loadFile('index.html');
+  win.loadFile("index.html");
 }
 
 app.whenReady().then(() => {
   createWindow();
 
   // macOS: re-open a window when the app is activated and none are open.
-  app.on('activate', () => {
+  app.on("activate", () => {
     if (BrowserWindow.getAllWindows().length === 0) {
       createWindow();
     }
@@ -23,8 +23,8 @@ app.whenReady().then(() => {
 
 // Quit the app once all windows are closed (except on macOS).
 // On a Mac, an app can keep running even with no windows open (ie. Spotify still in the menu bar with nothing visible)
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
